9a1e2595b0a9ba2f0a716603772faf4abd54650d
andromedagalua <andromedagalua@yandex.ru>
2023-09-22 14:01:08 +0300

    rgbrhjgrb start message

--- src/main/java/ru/idfedorov09/telegram/bot/fetcher/AdminCommandsFetcher.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/fetcher/AdminCommandsFetcher.kt
@@ -47,7 +47,6 @@ class AdminCommandsFetcher(
         // команда для того чтобы стать админом. Работает только с экспом!
         if (exp.allowSpecialCommands && message == "/be_admin") {
             userRepository.save(user.copy(isAdmin = true))
-            bot.execute(SendMessage(chatId, "теперь ты бабуин, даун"))
         }
 
         // от админов разрешена только одна команда - старт опроса. Если это не она - скипаем фетчер
@@ -55,7 +54,6 @@ class AdminCommandsFetcher(
         redisService.setLastPollDate(LocalDateTime.now(ZoneId.of("Europe/Moscow")))
 
         // TODO: здесь пройтись по пользователям и разослать им сообщение о начале опроса о занятии
-        // bot.execute(SendMessage(chatId, "БУДЬ БАБУИННРОООООООМММ:::::: $update"))
         userRepository.findAll().forEach{ user ->
             user.tui?.let {
                 userRepository.save(user.copy(currentQuestion = 1))
